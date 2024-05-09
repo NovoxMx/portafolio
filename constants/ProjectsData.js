3082a3239331368995cedffe7214369a7613c659
NovoxMx <A00835132@tec.mx>
2024-05-09 11:07:23 -0700

    si

--- constants/ProjectsData.js
+++ constants/ProjectsData.js
@@ -1,16 +1,20 @@
 export const ProjectsData = [
     {
         projectName: "Portal para la gestión de Viaticos",
+        githubUrl: "https://github.com/ximenalopezm/Pepe",
         projectImage: {
             imageUrl: "https://res.cloudinary.com/daoth80ez/image/upload/v1715198521/Media/juogo14hbiq70bxjroqh.png"
         },
+        projectDescription: "Portal para la gestión de viáticos, en el cual se pueden solicitar viáticos, aprobarlos, rechazarlos, y visualizar los viáticos solicitados y aprobados. Además, se pueden visualizar el historial de viaticos con las compra realizada en ese viaje.",
         techs: ["React", "Bootstrap", "Javascript", "Typescript", "NodeJS", "MySQL"]
     },
     {
         projectName: "Aplicación para el manejo de incidentes y bitácoras",
+        githubUrl: "https://github.com/El-MeGaMer/BAMX_APP_TC2007B.501",
         projectImage: {
             imageUrl: "https://res.cloudinary.com/daoth80ez/image/upload/v1715198567/Media/x5u3j3oppz2mktslclya.png"
         },
+        projectDescription: "Aplicación para el manejo de incidentes y bitácoras, en el cual se pueden reportar incidentes, visualizar incidentes reportados, visualizar bitácoras y a que area pertenecen, y visualizar el historial de bitacoras.",
         techs: ["ReactJS", "ReactNative", "TailwindCSS", "Javascript", "Typescript", "NodeJS", "MySQL"]
     },
     {
--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,5 @@
 import { AppsData } from "./AppsData";
-import { NewsData } from "./NewsData";
 import { SkillsData } from "./SkillsData";
 import { ProjectsData } from "./ProjectsData";
 
-export { AppsData, NewsData, SkillsData, ProjectsData }
+export { AppsData, SkillsData, ProjectsData }
