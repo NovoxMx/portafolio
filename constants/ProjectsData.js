1a61df5326aa2f1825cbc4d806f3f754ca2b7c68
NovoxMx <A00835132@tec.mx>
2024-05-23 21:41:08 -0700

    Actualizar datos de proyectos

--- constants/ProjectsData.js
+++ constants/ProjectsData.js
@@ -1,8 +1,6 @@
 export const ProjectsData = [
     {
         projectName: "Portal para la gestión de Viaticos",
-        githubUrl: "https://github.com/NovoxMx/TC2005B-front-end/tree/main",
-        figmaUrl: "https://www.figma.com/design/u2OB1Z4sKELBbcnrYIp2og/Mock-up-SCHAFER?node-id=0%3A1&t=ArapUYfcsf9SpUfi-1",
         projectImage: {
             imageUrl: "https://res.cloudinary.com/daoth80ez/image/upload/v1715198521/Media/juogo14hbiq70bxjroqh.png"
         },
@@ -11,8 +9,6 @@ export const ProjectsData = [
     },
     {
         projectName: "Aplicación para el manejo de incidentes y bitácoras",
-        githubUrl: "https://github.com/El-MeGaMer/BAMX_APP_TC2007B.501",
-        figmaUrl: "https://www.figma.com/design/X5TolFIBrOBs9fzbWYdee9/Aplicacion-Banco?node-id=0%3A1&t=SK5bRwYWbXrd1amL-1",
         projectImage: {
             imageUrl: "https://res.cloudinary.com/daoth80ez/image/upload/v1715198567/Media/x5u3j3oppz2mktslclya.png"
         },
@@ -21,8 +17,6 @@ export const ProjectsData = [
     },
     {
         projectName: "Nasa Copilot",
-        githubUrl: "https://github.com/ximenalopezm/Pepe",
-        figmaUrl: "https://www.figma.com/design/yMUS4xoNKcrjXsLIhHX4tS/Figma-basics?node-id=1669%3A162202&t=G9Mx8dIRqduMoFGb-1",
         projectImage: {
             imageUrl: "https://res.cloudinary.com/daoth80ez/image/upload/v1715198675/Media/nojqxj9k0uuyaqxnzquy.png"
         },
